40665b2e0ab3ab97471cc47e2a99d1fe29f5da3b
Sakoda2269 <Ksakoda.2269@gmail.com>
2025-01-19 20:22:09 +0900

    サイドバーに色を付けた

--- aisns_frontend/app/(sns)/layout.js
+++ aisns_frontend/app/(sns)/layout.js
@@ -4,15 +4,20 @@ import { useEffect, useState } from "react";
 
 import { FaCircleUser } from "react-icons/fa6";
 import { FaHome } from "react-icons/fa";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Layout({ children }) {
 
     const router = useRouter();
+    const path = usePathname().split("/");
+    let first = "";
+    if(path.length > 1) {
+        first = path[1];
+    }
 
     const accountPage = () => {
         const id = sessionStorage.getItem("id")
-        if(id != null) {
+        if (id != null) {
             router.push("/accounts/" + id + "?myaccount=true")
         } else {
             router.push("/login")
@@ -25,12 +30,14 @@ export default function Layout({ children }) {
                 hello
             </header> */}
             <div className="row">
-                <div className="col-1 container mt-3" style={{ display: "flex", flexDirection: "column", borderRight: "1px solid black", height: "95vh", alignItems: "center" }}>
-                    <div style={{ padding: "10px" }}>
-                        <Link href="/home"><button className="btn"><FaHome size={30} /></button></Link>
-                    </div>
-                    <div style={{ padding: "10px" }}>
-                       <button className="btn" onClick={accountPage}><FaCircleUser size={30} /></button>
+                <div className="col-1 container mt-3">
+                    <div style={{ position: "fixed", display: "flex", flexDirection: "column", borderRight: "1px solid black", height: "95vh", alignItems: "center" }}>
+                        <div style={{ padding: "10px" }}>
+                            <Link href="/home"><button className="btn"><FaHome size={30} color={first == "home" ? "blue":"black"}/></button></Link>
+                        </div>
+                        <div style={{ padding: "10px" }}>
+                            <button className="btn" onClick={accountPage}><FaCircleUser size={30} color={first == "accounts" ? "blue":"black"}/></button>
+                        </div>
                     </div>
                 </div>
                 <div className="col-9">
